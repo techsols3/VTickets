7e4e8ff53848135f5c18675831f6cec8e4abcb07
Muhammad Mubashir <techsolutionsofficial@outlook.com>
2021-12-30 20:54:49 +0500

    Initial Commit

---
+++ VTickets/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VTickets.Data;
+
+[assembly: HostingStartup(typeof(VTickets.Areas.Identity.IdentityHostingStartup))]
+namespace VTickets.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
